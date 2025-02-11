6f3172762b0a2e8a03a0f043dc0d88bbbd51128a
m-aksoy-altug <m.aksoy.altug@gmail.com>
2025-02-12 09:30:48 +1100

    Adding /uploadZip endpoint: upload a zip file and extract it.

--- src/main/java/com/springfile/controller/FileTransferController.java
+++ src/main/java/com/springfile/controller/FileTransferController.java
@@ -1,5 +1,6 @@
 package com.springfile.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,6 +8,8 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -54,6 +57,51 @@ public class FileTransferController {
 
 	    return ResponseEntity.ok("Uploaded files: " + String.join(", ", fileNames));
 	}
+	
+	@PostMapping("/uploadZip")
+	public ResponseEntity<String> uploadZip(@RequestParam("file") MultipartFile zipFile) throws IOException {
+		
+		if (zipFile.isEmpty()) {
+            return ResponseEntity.badRequest().body("No zip file provided!");
+        }
+	    
+        Path uploadPath = Paths.get(UPLOAD_DIR);
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+        
+        Path zipPath = uploadPath.resolve(zipFile.getOriginalFilename());
+        Files.copy(zipFile.getInputStream(), zipPath, StandardCopyOption.REPLACE_EXISTING);
+
+        extractZipFile(zipPath.toString(), UPLOAD_DIR);
+
+        Files.delete(zipPath);
+
+        return ResponseEntity.ok("ZIP file extracted successfully!");
+	}
+	
+	private void extractZipFile(String zipFilePath, String destDir) throws IOException {
+        File destDirectory = new File(destDir);
+        if (!destDirectory.exists()) {
+            destDirectory.mkdirs();
+        }
+
+        try (ZipInputStream zipInputStream = 
+        		new ZipInputStream(Files.newInputStream(Paths.get(zipFilePath)))) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                Path newPath = Paths.get(destDir, zipEntry.getName());
+                if (zipEntry.isDirectory()) {
+                    Files.createDirectories(newPath);
+                } else {
+                    Files.createDirectories(newPath.getParent());
+                    Files.copy(zipInputStream, newPath, StandardCopyOption.REPLACE_EXISTING);
+                }
+                zipInputStream.closeEntry();
+            }
+        }
+    }
+
 	
 	@PostMapping("/upload")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
